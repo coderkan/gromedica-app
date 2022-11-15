8d7635bacb4ec434ad58b49d4e08f302ea9ecfef
erkan.guzeler <erkan.guzeler@pinsoft.ist>
2022-11-15 21:19:30 +0300

    initialize commit

---
+++ src/test/java/com/nl/gerimedica/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nl.gerimedica.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
